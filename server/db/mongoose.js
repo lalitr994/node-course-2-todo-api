e33fe0d012726783754205dbc4a0b8f542c03368
lalitr994 <lalit.rajput@jadeglobal.com>
2018-04-16 12:31:59 +0530

    database user change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://todoapp:Taxino9211#@ds239587.mlab.com:39587/todoapp');
+mongoose.connect('mongodb://todoapp:lalit123@ds239587.mlab.com:39587/todoapp');
 
 module.exports = {mongoose}
