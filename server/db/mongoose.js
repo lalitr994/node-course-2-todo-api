b86d51b6f7e8e34f812fc7cd82b76f01a719d240
lalitr994 <lalit.rajput@jadeglobal.com>
2018-04-16 13:02:02 +0530

    process env chsnge

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://todoapp:lalit123@ds239587.mlab.com:39587/todoapp');
+
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://todoapp:lalit123@ds149268.mlab.com:49268/todoapp'
+  };
+
+
+mongoose.connect(mongoose.connect( db.localhost || db.mlab));
 
 module.exports = {mongoose}
