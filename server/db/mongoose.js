95ed7aa63cff895f9431fdf3192479b6ee8aa928
lalitr994 <lalit.rajput@jadeglobal.com>
2018-04-16 14:49:09 +0530

    d

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,11 +7,11 @@ mongoose.Promise = global.Promise;
 //     localhost: 'mongodb://localhost:27017/TodoApp',
 //     mlab: 'mongodb://<user>:<pass>@ds149268.mlab.com:49268/todoapp'
 //   };
-//   mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
-mongoose.connect('mongodb://todoapp:lalit123@ds239587.mlab.com:39587/todoapp');
+// mongoose.connect('mongodb://todoapp:lalit123@ds239587.mlab.com:39587/todoapp');
 
 module.exports = {mongoose}
